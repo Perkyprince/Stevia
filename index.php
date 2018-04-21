6a63735199dd32848487f37520446580c15b988b
Princewill Herberts <princewillherberts@gmail.com>
2018-04-21 19:03:27 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 TRANSITIONAL//EN">
+<html>
+     <head>
+      <title> HNG INTERNSHIP 4 </title>
+        <link rel="stylesheet" href="style.css">
+     </head>
+
+     <body>
+
+      <!-- NAVIGATION BAR -->
+<nav class="navbar navbar-default navbar-fixed-top">
+       <div class="navbar-brand"><h1>hotels.ng</h1></div>
+      </nav><!--navbar -->
+
+
+     	<div class="container"><h1 class="main">HNG Internship 4</h1>
+        <p>Become a better software developer</p>
+
+<div class="time">
+  <?php
+  date_default_timezone_set("Africa/Lagos");
+  echo  date("h:ia") . "<br>";
+  ?>
+</div>
+<div class="date">
+  <?php
+  date_default_timezone_set("Africa/Lagos");
+  echo date("l  d/m/Y") . "<br>";
+  ?>
+</div>
+</div>
+    </body>
+
+</html>
